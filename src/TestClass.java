b26c369e3dfcce5c395e8d97f3f6e929454b937e
Yubraj Pokharel <yubraj.pokharel555@gmail.com>
2019-08-08 00:08:23 -0500

    Initial commit

---
+++ src/TestClass.java
@@ -0,0 +1,12 @@
+
+
+public class TestClass {
+
+    public static void main(String args[]){
+        var num1 = "hello";
+        var num2 = "scala lol :D";
+
+        System.out.println(num1 + " " + num2);
+    }
+
+}
